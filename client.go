68242bf07747ff3eeccdf1e8f8abf2ea7e8bc934
Mikhail Kirillov <mikkirillov@yandex.ru>
2019-06-04 16:36:17 +0300

    first version client

---
+++ client.go
@@ -0,0 +1,89 @@
+package witai
+
+// @author  Mikhail Kirillov <mikkirillov@yandex.ru>
+// @version 1.000
+// @date    2019-06-04
+// @comment api doc https://wit.ai/docs/http/20170307
+
+import (
+	"crypto/tls"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+type ConfVal struct {
+	Name string  `json:"value"`
+	Conf float64 `json:"confidence"`
+}
+
+type Entities struct {
+	Intent []*ConfVal `json:"intent"`
+}
+
+type Response struct {
+	MsgId    string `json:"msg_id"`
+	Query    string `json:"_text"`
+	Entities Entities
+}
+
+type Client struct {
+	token   string
+	Timeout time.Duration
+}
+
+func New(token string) *Client {
+
+	return &Client{
+		token:   token,
+		Timeout: time.Second * 10,
+	}
+
+}
+
+func (c *Client) Message(txt string) ([]*ConfVal, error) {
+
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+
+	ua := &http.Client{
+		Timeout:   c.Timeout,
+		Transport: tr,
+	}
+
+	tr.DisableKeepAlives = true
+
+	req, err := http.NewRequest("GET", "https://api.wit.ai/message?v=20170307&q="+url.QueryEscape(txt), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Authorization", "Bearer "+c.token)
+
+	resp, e := ua.Do(req)
+	if e != nil {
+		return nil, e
+	}
+
+	if resp.StatusCode != 200 {
+		cont, _ := ioutil.ReadAll(resp.Body)
+		return nil, errors.New("wit.ai error: " + string(cont))
+	}
+
+	body, e1 := ioutil.ReadAll(resp.Body)
+	if e1 != nil {
+		return nil, errors.New("wit.ai error: " + e1.Error())
+	}
+
+	var r Response
+
+	if e := json.Unmarshal(body, &r); e != nil {
+		return nil, errors.New("wit.ai error: " + e.Error())
+	}
+
+	return r.Entities.Intent, nil
+}
